2d59f1e3959295e41dab7e503ecc6bcf4fd80927
Progamer10526 <7000886@spsd.sk.ca>
2025-09-12 14:09:24 -0600

    demo mouse and key presses

--- second P5/sketch.js
+++ second P5/sketch.js
@@ -4,29 +4,56 @@
 //
 // Fun user events 
 
-let circleColor = false;
+let circleColor = true;
+let currentColor = "white";
+let spot;
+let circleSize = 90;
+let rectSize = 300;
+let y;
+let fontSize = 50;
 
 function setup() {
   createCanvas(400, 400);
+  spot = width / 2
+  y = height * 0.75
 }
 
 function draw() {
   background(220);
+  movement();
   challenge(); // coordunate system challenge
+  mouseReport();
+}
+
+function movement(){
+  if(keyIsDown(RIGHT_ARROW)) spot += 5;
+  else if(keyIsDown(LEFT_ARROW)) spot -= 5;
+  if (keyIsDown(UP_ARROW)) y -=5;
+  else if(keyIsDown(DOWN_ARROW)) y +=5;
 }
 
 function keyPressed(){
-  if(circleColor){ circleColor = false;} else circleColor = true;}
-
+  if(keyCode === 71) currentColor = "green";
+  else if(keyIsDown(89) && keyIsDown(SHIFT)) currentColor = "yellow";
+}
 
 function challenge(){
-  let circleSize = 90
-  strokeWeight(3);
+  
+  strokeWeight(20);
   noFill();
-  if(circleColor){fill(20,50,90);}
+  if(circleColor){fill(currentColor);}
   circle(width , height ,circleSize);
   circle(width / 2,height/ 2,circleSize);
   circle(width , 0 ,circleSize);
   circle(0 , height  ,circleSize);
   circle(0 , 0 ,circleSize);
+  rect(spot - (rectSize / 2),y, rectSize,50,20,20,20,20);
+}
+
+function mouseReport(){
+  textSize(fontSize);
+  text(mouseX + "x, " + mouseY + "y, " + mouseIsPressed, mouseX,mouseY);
+}
+function mouseReleased(){
+  fontSize = random(20,60);
 }
