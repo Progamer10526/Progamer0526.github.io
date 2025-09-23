932ac8045ff1f53dcfaef933d661ab1fe7609a12
Progamer10526 <7000886@spsd.sk.ca>
2025-09-22 21:57:38 -0600

    Update sketch.js

--- First assignment/sketch.js
+++ First assignment/sketch.js
@@ -34,13 +34,8 @@ charY = height/2
 let size = 70;
 let shift = size /2;
 let leg = size*0.08
+control();
 stroke("black")
-if(keyIsDown(RIGHT_ARROW)){
-  x += 10
-}
-else if(keyIsDown(LEFT_ARROW)){
-  x-= 10
-}
 strokeWeight(0);
 fill("#AAFFAA");
 rect(x - shift, y - shift, size, size,size,size,0,0);
@@ -53,6 +48,23 @@ fill("black");
 circle(x- (leg*3.5), y - (shift/30) ,leg);
 circle(x+ (leg*3.5), y - (shift/30) ,leg);
 }
+
+function control(){
+  if(keyIsDown(RIGHT_ARROW)){
+  x += 10
+}
+else if(keyIsDown(LEFT_ARROW)){
+  x-= 10
+}
+if(keyIsDown(UP_ARROW)){
+  y -= 10
+}
+else if(keyIsDown(DOWN_ARROW)){
+  y+= 10
+}
+}
+
+
 
 function drawScene(){ //Draws the first scene
   background("skyblue");
@@ -93,6 +105,8 @@ function drawTrees(){ // draws the 5 trees on screen
   let leaves = width * 0.17;
   let loghieght = log *4;
   let treeAmount =5
+  let upshift = loghieght* 0.2 +40;
+  let upshift2 = upshift+65;
 let treex 
 let secondx = (width/treeAmount) 
 treex = secondx/1.6
@@ -102,15 +116,25 @@ treex = secondx/1.6
   rect(secondx *tree - treex,base + 10 - loghieght,log,loghieght);
   fill("green")
   leaves = width * 0.17;
-  triangle(secondx *tree - leaves/2.7- treex , base - 40, secondx*tree + leaves/1.7- treex ,base-40, secondx*tree + log/2- treex, base - 100)
+  triangle(secondx *tree - leaves/2.7- treex , base - upshift, secondx*tree + leaves/1.7- treex ,base-upshift, secondx*tree + log/2- treex, base - upshift2)
+  upshift = upshift + 40;
+  upshift2 = upshift2 + 40;
   leaves = leaves*0.6
-  triangle(secondx *tree- leaves/2- treex, base - 80, secondx*tree + leaves/2- treex + log ,base-80, secondx*tree + log/2- treex, base - 140)
+  triangle(secondx *tree- leaves/2- treex, base - upshift, secondx*tree + leaves/2- treex + log ,base-upshift, secondx*tree + log/2- treex, base - upshift2)
+  upshift = upshift + 40;
+  upshift2 = upshift2 + 40;
   leaves = leaves*0.7
-  triangle(secondx*tree - leaves/2- treex, base - 120, secondx*tree + leaves/2- treex + log ,base-120, secondx*tree + log/2- treex, base - 180)
+  triangle(secondx*tree - leaves/2- treex, base - upshift, secondx*tree + leaves/2- treex + log ,base-upshift, secondx*tree + log/2- treex, base - upshift2)
+  upshift = upshift + 40;
+  upshift2 = upshift2 + 40;
   leaves = leaves*0.7
-  triangle(secondx *tree- leaves/2- treex, base - 160, secondx*tree + leaves/2- treex + log ,base-160, secondx*tree + log/2- treex, base - 220)
+  triangle(secondx *tree- leaves/2- treex, base - upshift, secondx*tree + leaves/2- treex + log ,base-upshift, secondx*tree + log/2- treex, base - upshift2)
+  upshift = upshift + 40;
+  upshift2 = upshift2 + 40;
   leaves = leaves*0.7
-  triangle(secondx*tree - leaves/2- treex, base - 200, secondx*tree + leaves/2- treex + log ,base-200, secondx*tree + log/2- treex, base - 260)
+  triangle(secondx*tree - leaves/2- treex, base - upshift, secondx*tree + leaves/2- treex + log ,base-upshift, secondx*tree + log/2- treex, base - upshift2)
+  upshift = loghieght* 0.2 +40;
+  upshift2 = upshift+65;
   }
 }
 
