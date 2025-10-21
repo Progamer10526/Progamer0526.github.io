ed3546583509b160280b5b4b1797296d9eaa7723
Progamer10526 <7000886@spsd.sk.ca>
2025-10-21 14:09:55 -0600

    yes

--- car thingamabob/sketch.js
+++ car thingamabob/sketch.js
@@ -8,32 +8,76 @@ let hwight;
 let eastbound = []//20
 let westbound = []
 let test
+let laneran
+let carspawn = 0
+let caretyperan = 0
+let randomchance
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  hwight = height / 6;
-  test = new Car(1,1,2);
+  hwight = height / 8;
+
+  for (let cars = 0; cars <= 20; cars++) {
+    laneran = int(random(1, 3))
+    caretyperan = int(random(1, 3))
+    eastbound.push(new Car(laneran, caretyperan, 1))
+  }
+  for (let cars = 0; cars <= 20; cars++) {
+    laneran = int(random(1, 3))
+    caretyperan = int(random(1, 3))
+    westbound.push(new Car(laneran, caretyperan, 2))
+  }
 }
 
 function draw() {
   background(10, 160, 10);
   road();
-  test.display();
+  carspawn = int(random(1, 21))
+  for (let cars of eastbound) {
+    randomchance = int(random(0, 101))
+    if (randomchance === 1){
+      cars.speedUp()
+    }
+    else if (randomchance === 4){
+      cars.speeddown()
+    }
+    else if (randomchance === 8){
+      cars.changeColor()
+    }
+    cars.display()
+    cars.move()
+  }
+  for (let cars of westbound) {
+    randomchance = int(random(0, 101))
+    if (randomchance === 1){
+      cars.speedUp()
+    }
+    else if (randomchance === 4){
+      cars.speeddown()
+    }
+    else if (randomchance === 8){
+      cars.changeColor()
+    }
+    cars.display()
+    cars.move()
+  }
+
+
 }
 
 function road() {
   fill(0);
+  rect(0, hwight * 5, width, hwight);
+  fill(0);
   rect(0, hwight * 4, width, hwight);
+  laneLine(5, 4);
   fill(0);
   rect(0, hwight * 3, width, hwight);
-  laneLine(4,4);
+  laneLine(4, 1);
   fill(0);
   rect(0, hwight * 2, width, hwight);
-  laneLine(3,1);
-  fill(0);
-  rect(0, hwight * 1, width, hwight);
-  laneLine(2,2);
+  laneLine(3, 2);
 
 }
 
@@ -41,7 +85,7 @@ function laneLine(l, lane) {
   let x1 = 0
   let length = width / 24;
   if (lane === 1) {
-    length = width /18
+    length = width / 18
     fill(225, 225, 0);
     for (let x = length / 2; x <= width; x += length) {
       if (x1 % 2 === 0) {
@@ -61,52 +105,102 @@ function laneLine(l, lane) {
   }
 }
 
+function mousePressed(){
+  if()
+}
+
 class Car {
   constructor(lane, type, side) {
-    this.x1 = 0
-    this.x2 = width
+    this.carsize = width / 19
+    this.x1 = random(0, width)
+    this.x2 = random(0, width)
     this.side = side;
     this.lane = lane;
     this.type = type;
-    this.speed = 60;
+    this.speed = 20;
     this.c = color(random(0, 225), random(0, 225), random(0, 225))
   }
   move() {
-
+    if (this.side === 1) {
+      if (this.x1 <= width) {
+        this.x1 += this.speed;
+      }
+      else this.x1 = 0;
+    }
+    else if (this.side === 2) {
+      if (this.x2 >= 0) {
+        this.x2 -= this.speed;
+      }
+      else this.x2 = width;
+    }
   }
 
   display() {
-    if(this.side === 1){
-      if(this.type === 1){
+    if (this.side === 1) {
+      if (this.type === 1) {
         fill(this.c)
-      rect(this.x1,(this.lane * hwight) + (hwight*2) +30, width/12,hwight - 50 )
-      fill(0)
-      rect(this.x1 + (width/12 - 40),(this.lane * hwight) + (hwight*2) +50, 20,hwight - 90  )
+        rect(this.x1, (this.lane * hwight) + (hwight * 3) + 30, this.carsize, hwight - 50)
+        fill(0)
+        rect(this.x1 + ((this.carsize / 5) * 3.7), (this.lane * hwight) + (hwight * 3) + 40, this.carsize / 8, hwight - 70)
       }
-      else if(type === 2){
-       fill(this.c)
-      rect(this.x1,(this.lane * hwight) + (hwight*2) +30, width/12,hwight - 50 )
-      fill(0)
-      rect(this.x1 + (width/12 - 40),(this.lane * hwight) + (hwight*2) +50, 20,hwight - 90  )
-
+      else if (this.type === 2) {
+        fill(this.c)
+        rect(this.x1, (this.lane * hwight) + (hwight * 3) + 30, this.carsize, hwight - 50)
+        fill(0)
+        rect(this.x1 + ((this.carsize / 5) * 3.7), (this.lane * hwight) + (hwight * 3) + 40, this.carsize / 8, hwight - 70)
+        rect(this.x1 + ((this.carsize / 5) - 10), (this.lane * hwight) + (hwight * 3) + 40, this.carsize / 2, hwight - 70)
       }
     }
-    else if(this.side === 2){
-      if(this.type === 1){
+    else if (this.side === 2) {
+      if (this.type === 1) {
         fill(this.c)
-      rect(this.x2,(this.lane * hwight) + (hwight*2) +30, -(width/12),hwight - 50 )
-      fill(0)
-      rect(this.x2 + (width/12 - 40),(this.lane * hwight) + (hwight*2) +50, 20,hwight - 90  )
+        rect(this.x2, (this.lane * hwight) + (hwight * 1) + 30, this.carsize, hwight - 50)
+        fill(0)
+        rect(this.x2 + ((this.carsize / 5) / 3.7), (this.lane * hwight) + (hwight * 1) + 40, this.carsize / 8, hwight - 70)
       }
-      else if(type === 2){
-       fill(this.c)
-      rect(this.x2,(this.lane * hwight) + (hwight*2) +30, width/12,hwight - 50 )
-      fill(0)
-      rect(this.x2 + (width/12 - 40),(this.lane * hwight) + (hwight*2) +50, 20,hwight - 90  )
+      else if (this.type === 2) {
+        fill(this.c)
+        rect(this.x2, (this.lane * hwight) + (hwight * 1) + 30, this.carsize, hwight - 50)
+        fill(0)
+        rect(this.x2 + ((this.carsize / 5) / 3.7), (this.lane * hwight) + (hwight * 1) + 40, this.carsize / 8, hwight - 70)
+        rect(this.x2 + ((this.carsize / 5) + 10), (this.lane * hwight) + (hwight * 1) + 40, this.carsize / 2, hwight - 70)
 
       }
     }
   }
 
 
+  speedUp() {
+    if (this.side === 1) {
+      if(this.speed <= 40){
+        this.speed += 5;
+      }
+    }
+    else if (this.side === 2) {
+      if(this.speed <= 40){
+        this.speed += 5;
+      
+    }
+  }}
+
+  speeddown(){
+    if (this.side === 1) {
+      if(this.speed >=20){
+        this.speed -= 5;}
+      
+    }
+    else if (this.side === 2) {
+      if(this.speed >=20){
+        this.speed -= 5;}
+      
+    }
+  }
+
+
+  changeColor(){
+    this.c = color(random(0, 225), random(0, 225), random(0, 225))
+  }
+
+
+  
 }
